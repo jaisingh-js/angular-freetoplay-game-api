eedcf1db5e52e13b93e4bc1fbdd98b702490ffe7
Jai singh <jaisingh0335@gmail.com>
2022-12-08 18:51:22 +0530

    deleted unnecessary file

---
+++ src/app/interfaces/game.ts
@@ -0,0 +1,14 @@
+export interface Game {
+    id?: number;
+    title: string;
+    genre: string;
+    platform: string;
+    publisher: string;
+    thumbnail: string;
+    short_description: string;
+    game_url: string;
+    freetogame_profile_url: string;
+    description: string;
+    screenshots: any;
+
+}
